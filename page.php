e05ab70755c459bf2ab48cf4d2d29146018de6b5
Lê Trọng Nhân <44769901+NathanNhan@users.noreply.github.com>
2023-08-22 21:01:44 +0700

    Sửa bài tập 1 | Thao tac vối page

--- page.php
+++ page.php
@@ -31,14 +31,42 @@
     <!-- About us = 6 -> Our history = 12 
     wp_get_post_parent_id(12) -> 6 -> hiển thị thanh sidebar bên phải 
     if wp_get_post_parent_id(6) = 0 -> hiển thị thanh sidebar bên phải  -->
+
+    <!-- Lấy các page con của page hiện tại -> kết quả trả về true -> page = page cha  -->
+    <?php
+      $layCacPageCon = get_pages(array( 
+        'child_of' => get_the_ID()
+      ));
+      // Nếu như page hiện tại là page con (có ID page cha) hoặc page hiện tại là page cha (chứa các page con) 
+      if($theParent OR $layCacPageCon) {
+        ?>
+        <div class="page-links">
+          <h2 class="page-links__title"><a href="<?php echo get_permalink($theParent); ?>"><?php echo get_the_title($theParent); ?></a></h2>
+          <ul class="min-list">
+            <!-- <li class="current_page_item"><a href="<?php echo site_url('/our-history'); ?>">Our History</a></li>
+            <li><a href="<?php echo site_url('/our-goal'); ?>">Our Goals</a></li> -->
+            <?php 
+               if($theParent) {
+                $idCuaPageCha = $theParent;
+               } else {
+                $idCuaPageCha = get_the_ID();
+               }
+               wp_list_pages( array( 
+                'title_li' => NULL, 
+                'child_of' => $idCuaPageCha
+               ) );
+            
+            ?>
+          </ul>
+        </div>
+
+        <?php 
+         
+      }
     
-    <div class="page-links">
-      <h2 class="page-links__title"><a href="<?php echo site_url('/about-us'); ?>">About Us</a></h2>
-      <ul class="min-list">
-        <li class="current_page_item"><a href="<?php echo site_url('/our-history'); ?>">Our History</a></li>
-        <li><a href="<?php echo site_url('/our-goal'); ?>">Our Goals</a></li>
-      </ul>
-    </div>
+    ?>
+
+    
    
 
     <div class="generic-content">

5b7cd1570ee03fd210a48dfcdaf48b370acebd0e
Lê Trọng Nhân <44769901+NathanNhan@users.noreply.github.com>
2023-11-24 21:11:05 +0700

    bai25

--- single-professors.php
+++ single-professors.php
@@ -15,6 +15,48 @@
                           <img src="<?php the_post_thumbnail_url(); ?>" alt="<?php the_title(); ?>">
                       </div>
                       <div class="two-thirds">
+                        <?php 
+                            $likeCount = new WP_Query(
+                              array( 
+                                'post_type' => 'like',
+                                'meta_query' => array( 
+                                  array( 
+                                    'meta_key' => 'professors_id', 
+                                    'compare' => '=', 
+                                    'value' => get_the_ID()
+                                  )
+                                )
+                              )
+                            );
+
+                            //Xử lý like icon 
+                            $status = "no";
+
+                            $existsLike = new WP_Query(
+                              array( 
+                                'author' => get_current_user_id(),
+                                'post_type' => 'like',
+                                'meta_query' => array( 
+                                  array( 
+                                    'meta_key' => 'professors_id', 
+                                    'compare' => '=', 
+                                    'value' => get_the_ID()
+                                  )
+                                )
+                              )
+                            );
+
+                            if($existsLike->found_posts) {
+                              $status = "yes";
+                            }
+                             
+
+                        ?>
+                          <span class="like-box" data-exists="<?php echo $status; ?>">
+                            <i class="fa fa-heart-o" aria-hidden="true"></i>
+                            <i class="fa fa-heart" aria-hidden="true"></i>
+                            <span class="like-count"><?php echo $likeCount->found_posts; ?></span>
+                          </span>
                           <?php the_content(); ?>
                       </div>
                     </div>
